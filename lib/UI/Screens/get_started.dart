f4defef48b9422d5808642aee9d9c514ac0252a4
Bassam <jawish4123@gmail.com>
2022-12-03 00:12:56 +0200

    .

--- lib/UI/Screens/get_started.dart
+++ lib/UI/Screens/get_started.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 class GetStartedScreen extends StatelessWidget {
   const GetStartedScreen({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
 
