0ea7f7cd2da8b2c592d2508b3e253dec719b5b9b
Bassam <jawish4123@gmail.com>
2023-01-13 19:46:09 +0200

    .

---
+++ lib/translations/codegen_loader.g.dart
@@ -0,0 +1,216 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> ar = {
+    "Home": "الصفحة الرئيسية",
+    "Categories": "التصنيفات",
+    "All": "الكل",
+    "Medicine": "طبية",
+    "Career": "مهنية",
+    "Psychology": "نفسية",
+    "Family": "عائلية",
+    "Management": "إدارة",
+    "Others": "أخرى",
+    "Available Services": "الخدمات المتاحة",
+    "Type: ": "نوع: ",
+    "Settings": "إعدادات",
+    "Language": "اللغة",
+    "English": "الإنجليزية",
+    "Arabic": "العربية",
+    "About us": "عنا",
+
+    "Favorties": "المفضلة",
+    "Your favorites": "المفضلات",
+
+    "Profile": "الصفحة الشخصية",
+    "My Role": "دوري",
+    "My Name": "اسمي",
+    "My Email": "الايميلي",
+    "My Phone": "رقم هاتفي",
+    "My services": "خدماتي",
+    "price": "السعر",
+    "your consultation": "استشارتي",
+    "My Address": "عنواني",
+    "country": "البلد",
+    "city": "المدينة",
+    "My Experience": "خبرتي",
+    "Description": "الوصف",
+    "My Secheduale": "جدول مواعيدي",
+    "Sunday":"الاحد",
+    "Monday":"الاثنين",
+    "Tuesday": "الثلاثاء",
+    "Wednesday":"الاربعاء",
+    "Thrusday":"الخميس",
+    "Friday": "الجمعة",
+    "Saturday": "السبت",
+    "Start Time": "وقت البداية",
+    "End Time": "وقت النهاية",
+    "My balance": "حسابي",
+    "Reservations": "الححجوزات",
+    "Log out": "تسجيل الخروج",
+
+  "Get Started": "أبدأ الان",
+  "Sign up by choosing your role." :"قم بالتسجيل عن طريق اختيار الدور الخاص بك",
+  "I'm a User" :"انا مستخدم",
+  "I'm an Expert" :"انا خبير",
+  "Login":"تسجيل الدخول",
+
+  "First Name" : "الاسم",
+  "Last Name" : "الكنية",
+  "Enter Email" : "ادخل الايميل",
+  "password" : "كلمة السر",
+  "confirm password" : "تأكيد كلمة السر",
+  "phone" : "رقم الهاتف",
+  "Sign up" : "تسجيل",
+
+  "Already have an account?" :"تمتلك حساب بالفعل ؟",
+  "Log in":"تسجيل الدخول",
+
+  "More information" : "المزيد من المعلومات",
+  "Choose the services you offer" : "اختر الخدمات التي تقدمها",
+  "Your Address" : "عنوانك",
+  "Your Experience" : "خبراتك",
+  "Select your available time" : "اختر الاوقات المتاح فيها",
+
+    "Continue":"استمرار",
+
+
+  "About" : "حول",
+  "services" : "الخدمات",
+  "Address" : "العنوان",
+  "Email" : "الايميل",
+
+  "Service Info" : "معلومات الخدمة",
+  "Service" : "الخدمة",
+  "Price" : "السعر",
+  "Expert" : "الخبير",
+  "Pick Date:" : "اختر اليوم",
+  "Available times:" : "الاوقات المتاحة",
+  "Make reservation" : "احجز الخدمة",
+
+    "Show Profile":"عرض الصفحة",
+
+    "You don't have an account?":"لا تملك حساب؟",
+    "Welcome Back..": "أهلا بعودتك..",
+  "Forget Password?":"نسيت كلمة المرور",
+    "Login as guest": "نسجيل الدخول كضيف",
+
+    "Register":"تسجيل حساب جديد",
+
+  "Search" : "البحث",
+  "Category" : "التصنيف"
+};
+static const Map<String,dynamic> en = {
+  "Home": "Home",
+  "Categories": "Categories",
+  "Welcome Back..": "Welcome Back..",
+
+  "All": "All",
+  "Medicine": "Medicine",
+  "About":"About",
+
+  "Career": "Career",
+  "Psychology": "Psychology",
+  "Family": "Family",
+  "Management": "Management",
+  "Others": "Others",
+  "Available Services": "Available Services",
+  "Type: ": "Type: ",
+  "Settings": "Settings",
+  "Language": "Language",
+  "English": "English",
+  "Arabic": "Arabic",
+  "About us": "About us",
+
+
+  "Continue":"Continue",
+
+  "Forget Password?":"Forget Password?",
+  "Your favorites" : "Your favorites",
+
+  "Register": "Register",
+  "Login as guest": "Login as guest",
+
+  "Profile": "Profile",
+  "My Role": "My Role",
+  "My Name": "My Name",
+  "My Email": "My Email",
+  "My Phone": "My Phone",
+  "My services": "My Services",
+  "price": "price",
+  "your consultation": "your consultation",
+  "My Address": "My Address",
+  "country": "country",
+  "city": "city",
+  "My Experience": "My Experience",
+  "Description": "Description",
+  "My Secheduale": "My Secheduale",
+  "Sunday":"Sunday",
+  "Monday":"Monday",
+  "Tuesday": "Tuesday",
+  "Wednesday":"Wednesday",
+  "Thrusday":"Thrusday",
+  "Friday": "Friday",
+  "Saturday": "Saturday",
+  "Start Time": "Start Time",
+  "End Time": "End Time",
+  "My balance": "My balance",
+  "Reservations": "Reservations",
+  "Log out": "Log out",
+
+"Get Started" :"Get Started",
+  "Sign up by choosing your role." :"Sign up by choosing your role.",
+  "I'm a User" :"I'm a User",
+  "I'm an Expert" :"I'm an Expert",
+  "Login":"Login",
+
+  "First Name" : "First Name",
+  "Last Name" : "Last Name",
+  "Enter Email" : "Enter Email",
+  "password" : "password",
+  "confirm password" : "confirm password",
+  "phone" : "phone",
+  "Sign up" : "Sign up",
+
+  "Already have an account?" :"Already have an account?",
+  "Log in":"Log in",
+
+  "More information" : "More information",
+  "Choose the services you offer" : "Choose the services you offer",
+  "Your Address" : "Your Address",
+  "Your Experience" : "Your Experience",
+  "Select your available time" : "Select your available time",
+
+  "services" : "services",
+  "Address" : "Address",
+  "Email" : "Email",
+  "Show Profile":"Show Profile",
+
+  "You don't have an account":"You don't have an account",
+  "Service Info" : "Service Info",
+  "Service" : "Service",
+  "Price" : "Price",
+  "Expert" : "Expert",
+  "Pick Date:" : "Pick Date",
+  "Available times:" : "Available times",
+  "Make reservation" : "Make reservation",
+
+  "Search" : "Search",
+  "Category" : "Category"
+
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ar": ar, "en": en};
+}
